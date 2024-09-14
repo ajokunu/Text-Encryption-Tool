e1aebc7c0d775eccc832bfca4062027e2b7be51b
unknown <lindahlaaron@yahoo.com>
2024-09-14 19:27:21 -0400

    Wrote the key generation function more later Rust uses -> to specify return types, which is different from some other languages that might use : or put the return type before the function name. cd exit

--- Text Encryption/Text Encryption Main.rs
+++ Text Encryption/Text Encryption Main.rs
@@ -4,3 +4,8 @@ use rand::rngs::OsRng;
 use aes_gcm::{Aes256Gcm, Key, Nonce};
 use aes_gcm::aead::{Aead, NewAead};
 use base64::{encode, decode};
+
+fn generate_encryption_kp(Vec<u8>) -> (Vec<u8>) {
+    let (public_key, secret_key) = kyber768::keypair();
+    (public_key.as_bytes().to_vec(), secret_key.as_bytes().to_vec())
+}
