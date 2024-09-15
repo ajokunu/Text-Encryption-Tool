c47fc074c5797426b3e70433dcf71abb71a7b87d
unknown <lindahlaaron@yahoo.com>
2024-09-15 18:33:03 -0400

    added function

--- Text Encryption/Text Encryption Main.rs
+++ Text Encryption/Text Encryption Main.rs
@@ -9,3 +9,9 @@ fn generate_encryption_kp(Vec<u8>) -> (Vec<u8>) {
     let (public_key, secret_key) = kyber768::keypair();
     (public_key.as_bytes().to_vec(), secret_key.as_bytes().to_vec())
 }
+
+fn encapsulation(public_key: &[u8]) -> ( Vec<u8>, Vec<u8>) {
+    let public_key = kyber768::PublicKey::from_bytes(public_key).unwrap();
+    let(ciphertext, shared secret) = kyber768::encapsulate(&public_key);
+    (ciphertext.as_bytes().to_vec(), shared_secret.as_bytes().to_vec())
+}
