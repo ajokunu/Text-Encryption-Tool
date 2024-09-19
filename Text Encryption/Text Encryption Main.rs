d6354272121400364d3977192f10b6605f1bee65
unknown <lindahlaaron@yahoo.com>
2024-09-19 07:21:01 -0400

    added the main functions need to add nonce function

--- Text Encryption/Text Encryption Main.rs
+++ Text Encryption/Text Encryption Main.rs
@@ -22,3 +22,44 @@ fn decapsulation(Ciphertext: &[u8], secret: &[u8]) -> Vec<u8>) {
     let shared_secret = kyber768::decapsulate(&Ciphertext, &secret_key);
     shared_secret.as_bytes().to_vec()
 }
+
+fn encrypt(plaintext: &str, shared_secret: &[u8]) -> String {
+    let key = Key::from_slice(shared_secret);
+    let cipher = Aes256Gcm::new(key);
+    let nonce = Nonce::from_slice(b"unique nonce"); 
+    let ciphertext = cipher.encrypt(nonce, plaintext.as_bytes()).unwrap();
+    encode(ciphertext)
+}
+
+fn decrypt(ciphertext: &str, shared_secret: &[u8]) -> String {
+    let key = Key::from_slice(shared_secret);
+    let cipher = Aes256Gcm::new(key);
+    let nonce = Nonce::from_slice(b"unique nonce");
+    let ciphertext = decode(ciphertext).unwrap();
+    let plaintext = cipher.decrypt(nonce, ciphertext.as_ref()).unwrap();
+    String::from_utf8(plaintext).unwrap()
+}
+
+fn main() {
+    let plaintext = "This is an encrypted message, decrypt to decipher";
+
+    // Generate keypair
+    let (public_key, secret_key) = generate_keypair();
+
+    // Encapsulate to get ciphertext and shared secret
+    let (ciphertext, alice_shared_secret) = encapsulate(&public_key);
+
+    // Decapsulate to get the same shared secret
+    let bob_shared_secret = decapsulate(&ciphertext, &secret_key);
+
+    assert_eq!(alice_shared_secret, bob_shared_secret, "Shared secrets don't match!");
+
+    // Encrypt and decrypt the message
+    let encrypted = encrypt(plaintext, &alice_shared_secret);
+    println!("Encrypted (base64): {}", encrypted);
+
+    let decrypted = decrypt(&encrypted, &bob_shared_secret);
+    println!("Decrypted: {}", decrypted);
+
+    assert_eq!(plaintext, decrypted, "Decryption failed!");
+}
